ee5b3f9849a117fb0a6afdefe581ca6ced62e51a
班曼丽 <maryban@qq.com>
2019-03-26 16:31:08 +0800

    file init

---
+++ Example/Pods/Target Support Files/MaryBanCategoryKit/MaryBanCategoryKit-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_MaryBanCategoryKit : NSObject
+@end
+@implementation PodsDummy_MaryBanCategoryKit
+@end
---
+++ Example/Pods/Target Support Files/Pods-MaryBanCategoryKit_Tests/Pods-MaryBanCategoryKit_Tests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_MaryBanCategoryKit_Tests : NSObject
+@end
+@implementation PodsDummy_Pods_MaryBanCategoryKit_Tests
+@end
